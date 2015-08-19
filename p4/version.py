d6487e6c97f4799481e333edf17bf0eb89d8efd5
Peter Foster <p.foster2@gmail.com>
2015-08-19 15:46:56 +0100

    version.py

--- p4/version.py
+++ p4/version.py
@@ -1,5 +1,5 @@
 versionNumberString = '1.0'   # a string that can be turned into a number
 versionNumber = float(versionNumberString)
-versionNumberModifier = ' [2015-06-25]'    # a string
+versionNumberModifier = ' [2015-08-19]'    # a string
 versionString = '%s%s' % (versionNumberString, versionNumberModifier)
-dateString = "25 June, 2015"
+dateString = "19 August, 2015"
