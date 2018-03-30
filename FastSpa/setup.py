cae3c855f426503be2331996344540dfb2e84f42
Peter Foster <p.foster2@gmail.com>
2018-03-30 19:16:48 +0100

    adjusted the setup.py to compile fastspa

--- FastSpa/setup.py
+++ FastSpa/setup.py
@@ -6,11 +6,19 @@ setup(name="fastSpa",
         Extension("fastspa", ['fastSpa.cpp'],
                   # Adjust the following to be able to find boost-python and, boost-numpy.
                   # On my mac, using Homebrew, I did not need include_dirs or library_dirs, as it was installed
-                  # in a usual location.
+                  # in a "usual" location.
                   # On my old Ubuntu box, I installed boost "in-place" (or rather I did not "install" it as such) 
                   # and so I had to specify both dirs.
-                  include_dirs = ["/usr/local/src/Boost/boost_1_66_0"],
-                  library_dirs = ["/usr/local/src/Boost/boost_1_66_0/stage/lib"],
+                  #include_dirs = ["/usr/local/src/Boost/boost_1_66_0"],
+                  #library_dirs = ["/usr/local/src/Boost/boost_1_66_0/stage/lib"],
+
+                  # Adjust and use this next line if you want to build with
+                  # statically linked libs.  You might want to do this if your
+                  # libs are in a non-standard location, and (although it
+                  # compiles well) when you run it it complains that it cannot
+                  # find the boost libs.  A related reason might be if you are
+                  # using more than one boost.
+                  #extra_link_args = ['-Wl,-rpath=/usr/local/src/Boost/boost_1_66_0/stage/lib' ],  # static build
                   libraries = ["boost_python3", "boost_numpy3"]   # needed and worked on both linux and mac
         )
     ])
