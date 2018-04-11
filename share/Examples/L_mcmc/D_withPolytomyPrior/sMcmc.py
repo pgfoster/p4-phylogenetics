088498a5a0a2d030e648a1d735587ebbb00854cb
Peter Foster <p.foster2@gmail.com>
2018-04-11 08:31:23 +0100

    Fixed polytomy Example

--- share/Examples/L_mcmc/D_withPolytomyPrior/sMcmc.py
+++ share/Examples/L_mcmc/D_withPolytomyPrior/sMcmc.py
@@ -11,10 +11,9 @@ m = Mcmc(t, nChains=1, runNum=0, sampleInterval=10, checkPointInterval=2000)
 # Set up a strong prior on polytomies
 if 1:
     m.prob.polytomy = 1.0
-    m.prob.brLen = 0.001
-    m.tunings.doPolytomyResolutionClassPrior = True  # False by default
+    m.polytomyUseResolutionClassPrior = True  # False by default
     import math
-    m.tunings.polytomyPriorLogBigC = math.log(10.)   # Default is zero
+    m.polytomyPriorLogBigC = math.log(10.)   # Default is zero
 
 m.run(4000)
 
