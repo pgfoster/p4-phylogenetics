127c51d230e6af71335ac8d029176f87a250cd17
Peter Foster <p.foster2@gmail.com>
2019-05-11 15:24:42 +0100

    Constraints can now handle bi-rooted constraint trees

--- p4/constraints.py
+++ p4/constraints.py
@@ -25,8 +25,6 @@ class Constraints(object):
       c = Constraints(tNames, constTree)
       t = p4.func.randomTree(taxNames=tName, constraints=c)
 
-    The constraint tree should not have a bifurcating root.
-
     You can pass a Constraints object to p4.func.randomTree() and
     Mcmc() to enforce constraints.
 
@@ -34,21 +32,17 @@ class Constraints(object):
 
     def __init__(self, taxNames, constraintTree):
 
-        if constraintTree.root.getNChildren() == 2:
-            raise P4Error(
-                'The constraint tree should not have a bifurcating root.')
         self.tree = constraintTree
         self.tree.taxNames = taxNames
-        self.tree.reRoot(self.tree.taxNames[0], moveInternalName=False)
         self.allOnes = 2 ** (self.tree.nTax) - 1
 
         self.tree.makeSplitKeys()
         self.constraints = []
-        internalsExceptTheFirst = [
-            n for n in self.tree.iterInternalsNoRoot()][1:]
-        for n in internalsExceptTheFirst:
+        self.tree.draw()
+        for n in self.tree.iterInternalsNoRoot():
             n.name = n.br.splitKey
-            self.constraints.append(n.br.splitKey)
+            if n.br.splitKey not in self.constraints:
+                self.constraints.append(n.br.splitKey)
         assert self.constraints, "No constraints?"
 
     def dump(self):
@@ -58,5 +52,5 @@ class Constraints(object):
             print('    %3i  %s' % (i, self.tree.taxNames[i]))
         print('constraints:')
         for i in self.constraints:
-            print(p4.func.getSplitStringFromKey(i, self.tree.nTax))
+            print(p4.func.getSplitStringFromKey(i, self.tree.nTax), i)
         self.tree.draw()
