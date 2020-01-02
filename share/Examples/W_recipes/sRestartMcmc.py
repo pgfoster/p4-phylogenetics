c8b33efb4b14b6382913aff4aa66e6a1bef23896
Peter Foster <p.foster2@gmail.com>
2020-01-02 11:03:26 +0700

    Tweaked recipe sRestartMcmc.py

--- share/Examples/W_recipes/sRestartMcmc.py
+++ share/Examples/W_recipes/sRestartMcmc.py
@@ -1,7 +1,15 @@
 # Restart an MCMC.
 read("d.nex")
 d = Data()
-m = func.unPickleMcmc(0, d)
+myRunNum = 0
+m = func.unPickleMcmc(myRunNum, d)
+
+# Settings made to the var module in the old MCMC are not present in
+# the pickled file, so you may need to set them again ---
+# var.PIVEC_MIN = 1.e-7
+# var.RATE_MIN = 1.e-7
+# var.BRLEN_MIN = 1.e-6
+# var.GAMMA_SHAPE_MIN = 0.1
 
 # You probably don't want to do this, but 
 # it is possible to change the mcmc here.
