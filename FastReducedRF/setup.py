829f2693c2ccc4e0b19a150a857cb90386e6c5f2
Peter Foster <p.foster2@gmail.com>
2020-06-01 14:17:15 +0100

    Refresh build of FastReducedRF C++ module

--- FastReducedRF/setup.py
+++ FastReducedRF/setup.py
@@ -5,14 +5,13 @@ setup(name="p4",
     ext_modules=[
         Extension("fastReducedRF", ['fastReducedRF.cpp'],
             # Adjust the following to be able to find pyublas, numpy, and boost.  Maybe need library_dirs as well?
-            include_dirs = ["/usr/local/lib/python3.6/site-packages/pyublas/include",
-                            "/usr/local/lib/python3.6/site-packages/numpy/core/include",
-                            "/usr/local/include"],    # worked on my mac
-            # include_dirs = ["/usr/local/lib/python3.4/dist-packages/pyublas/include",
-            #                 "/usr/local/lib/python3.4/dist-packages/numpy/core/include",
-            #                 ],    # worked on my ubuntu 14.04
-                  # libraries = ["boost_python"]   # or "boost_python-mt" if multi-threaded, on ubuntu
-                  # libraries = ["boost_python-py34"]   # worked on ubuntu 14.04.  No mt; mt not needed?
-                  libraries = ["boost_python3"]   # worked on my mac
+            #include_dirs = ["/usr/local/lib/python3.8/site-packages/pyublas/include",
+            #                "/usr/local/lib/python3.8/site-packages/numpy/core/include",
+            #                "/usr/local/include"],    # worked on my mac
+            include_dirs = ["/usr/local/lib/python3.7/site-packages/pyublas/include",
+                            "/usr/local/lib/python3.7/site-packages/numpy/core/include",
+                            "/usr/local/include"],    # worked on my ubuntu 16.04
+                  libraries = ["boost_python37"]   # worked on ubuntu 16.04.  No mt; mt not needed?
+                  #libraries = ["boost_python38"]   # worked on my mac
         )
     ])
