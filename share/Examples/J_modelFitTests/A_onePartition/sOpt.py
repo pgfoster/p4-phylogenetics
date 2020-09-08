b14d8388efe95ee4d5efc2da60c616ba4c25a45d
Peter Foster <p.foster2@gmail.com>
2020-09-08 20:59:22 +0100

    fixed a couple of examples

--- share/Examples/J_modelFitTests/A_onePartition/sOpt.py
+++ share/Examples/J_modelFitTests/A_onePartition/sOpt.py
@@ -2,7 +2,7 @@ read('noOpt.p4_tPickle')
 t = var.trees[0]
 read('d.nex')
 t.data = Data()
-t.optLogLike(newtAndBrentPowell=1, allBrentPowell=0, verbose=1)
+t.optLogLike(method="newtAndBrentPowell", verbose=1)
 t.model.dump()
 t.tPickle('opt')
 
