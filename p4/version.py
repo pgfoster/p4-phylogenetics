4df217171ea40e5be564b0e154a7a0a6e473f746
Peter Foster <p.foster2@gmail.com>
2021-04-20 11:41:00 +0100

    version bump

--- p4/version.py
+++ p4/version.py
@@ -1,4 +1,9 @@
-versionNumberString = '1.4'
-versionNumberModifier = ' 5742542'    # a string
-versionString = '%s%s' % (versionNumberString, versionNumberModifier)
-dateString = "14 July, 2020"
+versionNumberString = '1.5'
+versionNumberModifier = '9406eaf'    # a string 
+versionString = '%s.%s' % (versionNumberString, versionNumberModifier)
+dateString = "15 April, 2021"
+
+# versionNumberString = '1.4'
+# versionNumberModifier = ' 5742542'    # a string
+# versionString = '%s%s' % (versionNumberString, versionNumberModifier)
+# dateString = "14 July, 2020"
